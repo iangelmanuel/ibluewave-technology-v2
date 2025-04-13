24cd3365ba0acd332154dbc1961445dc5c58ab3a
iangelmanuel <iangelmanueldm@gmail.com>
2025-04-13 18:22:26 -0500

    Refactor header and navbar components for improved structure and styling
    - Updated Header.astro to use new logo and link components, adjusted layout for better responsiveness.
    - Refactored Navbar.astro to utilize a centralized navigation constant and improved link styling.
    - Added new site information constants including URL, address, phone, and email.
    - Adjusted imports in Layout.astro for consistent casing.
    - Enhanced index.astro to include new sections: Blog, About, Courses, and Contact.
    - Updated tsconfig.json to include section path aliases.
    - Created new contact form and contact info map components for better organization.
    - Implemented social media links and button components for reusability.
    - Added navigation constants for easier management of navigation links.
    - Developed new sections for Ordenar, About, Blog, Contact, Courses, and Hero with appropriate styling and structure.

---
+++ src/constant/navigation.ts
@@ -0,0 +1,6 @@
+export const navigation = [
+  { text: "Inicio", href: "/" },
+  { text: "Conocenos", href: "/sobre-nosotros" },
+  { text: "Escribenos", href: "/contacto" },
+  { text: "Nuestro blog", href: "/blog" }
+]
--- src/constant/site-info.ts
+++ src/constant/site-info.ts
@@ -2,5 +2,9 @@ export const siteInfo = {
   title: "iBluewave Technology",
   description:
     "iBlue Wave Technology es un proyecto liderado por Ricardo De La Torre Mejía, un buzo certificado por NAUI con amplia experiencia en la enseñanza de cursos de buceo en Colombia.",
-  image: "assets/images/logo.webp"
+  image: "assets/images/logo.webp",
+  url: "https://ibluewavetechnology.com",
+  address: "TV 44 #102-167",
+  phone: "+57 300 802 9002",
+  email: "ibluwavetechnology@gmail.com"
 }
