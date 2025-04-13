19ebb8983aec26e9600fecdd6d52094e91ddcdb6
iangelmanuel <iangelmanueldm@gmail.com>
2025-04-13 18:43:32 -0500

    feat: add new footer section and improve layout
    - Added a new section to the footer with a call to action for diving courses.
    - Imported Link component for navigation in the footer.
    - Updated Layout.astro to import ClientRouter from astro:transitions.
    - Removed unused Ordenar.astro section.
    - Minor adjustments in about.astro and courses.astro for consistency.
    - Reordered imports in hero.astro for better organization.
    - Enhanced global styles with smooth scrolling behavior.
    - Added astro-icon package for potential future use.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from "astro/config"
 
 import tailwindcss from "@tailwindcss/vite"
 
-import react from "@astrojs/react";
+import react from "@astrojs/react"
+
+import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +13,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(), icon()]
 })
