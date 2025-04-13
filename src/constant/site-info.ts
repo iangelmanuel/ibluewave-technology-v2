2a03fa563879abde3b2e00457a9fda1d0af5451b
iangelmanuel <iangelmanueldm@gmail.com>
2025-04-13 00:54:40 -0500

    feat(dom): add utility functions for selecting DOM elements
    - Implemented `$` function to select a single element by selector string.
    - Implemented `$$` function to select multiple elements by selector string.
    - Both functions support an optional context parameter for scoped queries.

--- src/constant/site-info.ts
+++ src/constant/site-info.ts
@@ -1,6 +1,6 @@
 export const siteInfo = {
   title: "iBlue Wave Technology",
   description:
-    "iBlue Wave Technology es un proyecto liderado por Ricardo De La Torre Mejía, un buzo certificado por NAUI con amplia experiencia en la enseñanza de cursos de buceo en Colombia. Principalmente opera en destinos icónicos como Santa Marta, Cartagena y San Andrés, ofreciendo una experiencia única para los amantes del mar. Además, Ricardo se especializa en fotografía marítima, capturando la belleza del mundo submarino. Su pasión por el océano y su dedicación a compartir sus conocimientos hacen de iBlue Wave Technology una referencia en el ámbito del buceo y la exploración marina.",
-  image: "/src/assets/images/logo.png"
+    "iBlue Wave Technology es un proyecto liderado por Ricardo De La Torre Mejía, un buzo certificado por NAUI con amplia experiencia en la enseñanza de cursos de buceo en Colombia.",
+  image: "assets/images/logo.webp"
 }
