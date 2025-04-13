8d8158315eb24983a5cd77acdf2e1fcbcb932c97
iangelmanuel <iangelmanueldm@gmail.com>
2025-04-13 04:43:42 -0500

    feat: update dependencies and add new components
    - Added new dependencies for React and Tailwind CSS integration.
    - Updated site information and SEO metadata in Seo.astro.
    - Created a new Header component with logo and navigation.
    - Implemented Hero section with animated text and logo.
    - Added Navbar component for site navigation.
    - Created utility Link component for consistent styling.
    - Introduced WordPullUp component for animated text display.
    - Updated global styles with custom properties and dark mode support.
    - Added new pages for blog, contact, special courses, and about us.

--- src/constant/site-info.ts
+++ src/constant/site-info.ts
@@ -1,5 +1,5 @@
 export const siteInfo = {
-  title: "iBlue Wave Technology",
+  title: "iBluewave Technology",
   description:
     "iBlue Wave Technology es un proyecto liderado por Ricardo De La Torre Mejía, un buzo certificado por NAUI con amplia experiencia en la enseñanza de cursos de buceo en Colombia.",
   image: "assets/images/logo.webp"
