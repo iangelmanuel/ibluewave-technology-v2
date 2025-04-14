273f24f108c9e6851f839383e4dc17ebd14373fe
iangelmanuel <iangelmanueldm@gmail.com>
2025-04-13 20:08:50 -0500

    feat: implement contact information and social media components with icons

---
+++ src/constant/social-media.ts
@@ -0,0 +1,15 @@
+export const socialMediaIcon = [
+  {
+    name: "social-media/facebook",
+    href: "https://www.facebook.com/delatorrerikrdo"
+  },
+  {
+    name: "social-media/instagram",
+    href: "https://www.instagram.com/ibluewavetechnology/"
+  },
+  { name: "social-media/x", href: "https://x.com/rikr21980" },
+  {
+    name: "social-media/youtube",
+    href: "https://www.youtube.com/@soyricardodelatorre"
+  }
+]
