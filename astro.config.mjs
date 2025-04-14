12ce3e2b1b7705a636056894e36942570dd84557
iangelmanuel <iangelmanueldm@gmail.com>
2025-04-14 17:17:27 -0500

    fix: update alias paths in Vite configuration to use fileURLToPath

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config"
+import { fileURLToPath } from "url"
 
 import tailwindcss from "@tailwindcss/vite"
-
 import react from "@astrojs/react"
-
 import icon from "astro-icon"
 
 // https://astro.build/config
@@ -13,15 +12,21 @@ export default defineConfig({
     plugins: [tailwindcss()],
     resolve: {
       alias: {
-        "@layouts/*": "./src/layouts/*",
-        "@pages/*": "./src/pages/*",
-        "@sections/*": "./src/sections/*",
-        "@components/*": "./src/components/*",
-        "@interfaces/*": "./src/interfaces/*",
-        "@styles/*": "./src/styles/*",
-        "@lib/*": "./src/lib/*",
-        "@constant/*": "./src/constant/*",
-        "@images/*": "./src/assets/images/*"
+        "@layouts": fileURLToPath(new URL("./src/layouts", import.meta.url)),
+        "@pages": fileURLToPath(new URL("./src/pages", import.meta.url)),
+        "@sections": fileURLToPath(new URL("./src/sections", import.meta.url)),
+        "@components": fileURLToPath(
+          new URL("./src/components", import.meta.url)
+        ),
+        "@interfaces": fileURLToPath(
+          new URL("./src/interfaces", import.meta.url)
+        ),
+        "@styles": fileURLToPath(new URL("./src/styles", import.meta.url)),
+        "@lib": fileURLToPath(new URL("./src/lib", import.meta.url)),
+        "@constant": fileURLToPath(new URL("./src/constant", import.meta.url)),
+        "@images": fileURLToPath(
+          new URL("./src/assets/images", import.meta.url)
+        )
       }
     }
   },
