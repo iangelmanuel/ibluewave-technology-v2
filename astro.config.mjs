7075f9b2cbbfdf74294639c567f80d7860fc3a1a
iangelmanuel <iangelmanueldm@gmail.com>
2025-04-14 15:08:26 -0500

    fix: standardize import statements and add alias resolution in Vite configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,30 @@
 // @ts-check
-import { defineConfig } from "astro/config"
+import { defineConfig } from "astro/config";
 
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from "@tailwindcss/vite";
 
-import react from "@astrojs/react"
+import react from "@astrojs/react";
 
-import icon from "astro-icon"
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@layouts/*": "./src/layouts/*",
+        "@pages/*": "./src/pages/*",
+        "@sections/*": "./src/sections/*",
+        "@components/*": "./src/components/*",
+        "@interfaces/*": "./src/interfaces/*",
+        "@styles/*": "./src/styles/*",
+        "@lib/*": "./src/lib/*",
+        "@constant/*": "./src/constant/*",
+        "@images/*": "./src/assets/images/*"
+      }
+    }
   },
 
   integrations: [react(), icon()]
-})
+});
