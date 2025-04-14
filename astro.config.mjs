9f6383a0b35e84a61ce99de6fb658ed4c902cad3
iangelmanuel <iangelmanueldm@gmail.com>
2025-04-14 18:57:01 -0500

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config"
-import path from "path"
-import { fileURLToPath } from "url"
 
-import tailwindcss from "@tailwindcss/vite"
 import react from "@astrojs/react"
+import tailwindcss from "@tailwindcss/vite"
 import icon from "astro-icon"
+
+import { fileURLToPath } from "url"
+import path from "path"
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
@@ -12,17 +13,16 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
     resolve: {
-      preserveSymlinks: true,
       alias: {
-        "@layouts": path.resolve(__dirname, "src/layouts"),
-        "@pages": path.resolve(__dirname, "src/pages"),
-        "@sections": path.resolve(__dirname, "src/sections"),
-        "@components": path.resolve(__dirname, "src/components"),
-        "@interfaces": path.resolve(__dirname, "src/interfaces"),
-        "@styles": path.resolve(__dirname, "src/styles"),
-        "@lib": path.resolve(__dirname, "src/lib"),
-        "@constant": path.resolve(__dirname, "src/constant"),
-        "@images": path.resolve(__dirname, "src/assets/images")
+        "@layouts": path.resolve(__dirname, "./src/layouts"),
+        "@pages": path.resolve(__dirname, "./src/pages"),
+        "@sections": path.resolve(__dirname, "./src/sections"),
+        "@components": path.resolve(__dirname, "./src/components"),
+        "@interfaces": path.resolve(__dirname, "./src/interfaces"),
+        "@styles": path.resolve(__dirname, "./src/styles"),
+        "@lib": path.resolve(__dirname, "./src/lib"),
+        "@constant": path.resolve(__dirname, "./src/constant"),
+        "@images": path.resolve(__dirname, "./src/assets/images")
       }
     }
   },
