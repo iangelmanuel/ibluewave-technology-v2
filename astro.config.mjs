17cc70663a5f29ab8578f89d5b3f4fdb057880f2
iangelmanuel <iangelmanueldm@gmail.com>
2025-04-14 15:32:27 -0500

    fix: remove unnecessary semicolons and standardize alias paths in Vite configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from "astro/config"
 
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"
 
-import react from "@astrojs/react";
+import react from "@astrojs/react"
 
-import icon from "astro-icon";
+import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,18 +13,18 @@ export default defineConfig({
     plugins: [tailwindcss()],
     resolve: {
       alias: {
-        "@layouts/*": "./src/layouts/*",
-        "@pages/*": "./src/pages/*",
-        "@sections/*": "./src/sections/*",
-        "@components/*": "./src/components/*",
-        "@interfaces/*": "./src/interfaces/*",
-        "@styles/*": "./src/styles/*",
-        "@lib/*": "./src/lib/*",
-        "@constant/*": "./src/constant/*",
-        "@images/*": "./src/assets/images/*"
+        "@layouts": "/src/layouts",
+        "@pages": "/src/pages",
+        "@sections": "/src/sections",
+        "@components": "/src/components",
+        "@interfaces": "/src/interfaces",
+        "@styles": "/src/styles",
+        "@lib": "/src/lib",
+        "@constant": "/src/constant",
+        "@images": "/src/assets/images"
       }
     }
   },
 
   integrations: [react(), icon()]
-});
+})
