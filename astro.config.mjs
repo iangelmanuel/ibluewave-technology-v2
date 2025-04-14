e7e6af1454cce7513fe9bbfbff6b95232f161e5d
iangelmanuel <iangelmanueldm@gmail.com>
2025-04-14 18:10:16 -0500

    fix: update import paths for Marquee component and Button in ContactForm; add Vercel configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
     resolve: {
+      preserveSymlinks: true,
       alias: {
         "@layouts": path.resolve(__dirname, "src/layouts"),
         "@pages": path.resolve(__dirname, "src/pages"),
