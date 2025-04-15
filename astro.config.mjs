e610693e023982d77ca6701b008d411ab99e01a6
iangelmanuel <iangelmanueldm@gmail.com>
2025-04-14 19:08:37 -0500

    chore: add pnpm workspace configuration with ignored built dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,13 @@
 import { defineConfig } from "astro/config"
 
-import react from "@astrojs/react"
 import tailwindcss from "@tailwindcss/vite"
+
+import react from "@astrojs/react"
 import icon from "astro-icon"
-
-import { fileURLToPath } from "url"
-import path from "path"
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        "@layouts": path.resolve(__dirname, "./src/layouts"),
-        "@pages": path.resolve(__dirname, "./src/pages"),
-        "@sections": path.resolve(__dirname, "./src/sections"),
-        "@components": path.resolve(__dirname, "./src/components"),
-        "@interfaces": path.resolve(__dirname, "./src/interfaces"),
-        "@styles": path.resolve(__dirname, "./src/styles"),
-        "@lib": path.resolve(__dirname, "./src/lib"),
-        "@constant": path.resolve(__dirname, "./src/constant"),
-        "@images": path.resolve(__dirname, "./src/assets/images")
-      }
-    }
+    plugins: [tailwindcss()]
   },
   integrations: [react(), icon()]
 })
