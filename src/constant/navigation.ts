5eea18de9efbb3c9e460f597aab5fe9aa342eda2
iangelmanuel <iangelmanueldm@gmail.com>
2025-04-25 23:49:10 -0500

    Fix typos and improve content consistency across various sections; add new icons for mission and vision; enhance blog layout and featured article section.

--- src/constant/navigation.ts
+++ src/constant/navigation.ts
@@ -1,6 +1,7 @@
 export const navigation = [
   { text: "Inicio", href: "/" },
   { text: "Sobre nosotros", href: "/sobre-nosotros" },
-  { text: "Blog", href: "/blog" },
+  { text: "Cursos especiales", href: "/cursos-especiales" },
+  // { text: "Blog", href: "/blog" },
   { text: "Contáctanos", href: "/contacto" }
 ]
